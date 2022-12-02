6dfed86c0ca0c413c1e820d08d506182c94fa89d
wzz <2838259857@qq.com>
2022-12-02 22:31:35 +0800

    登录

--- src/com/jr/servelet/Userervlet.java
+++ src/com/jr/servelet/Userervlet.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 public class Userervlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-            login(req,resp);
+        login(req,resp);
     }
 
     @Override
