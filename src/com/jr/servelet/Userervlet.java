84b03a4283ecdbbb30308d58246b51f041886946
wzz <2838259857@qq.com>
2022-12-02 17:09:16 +0800

    登录

--- src/com/jr/servelet/Userervlet.java
+++ src/com/jr/servelet/Userervlet.java
@@ -9,7 +9,14 @@ import java.io.IOException;
 public class Userervlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        req.setCharacterEncoding("utf-8"); //设置请求信息的字符为utf-8
+        resp.setCharacterEncoding("utf-8");
+        resp.setContentType("text/html;charset=UTF-8");
+        String str=req.getParameter("i");
+        while (str==null){
+            login(req,resp);
+        }
+
     }
 
     @Override
@@ -26,6 +33,14 @@ public class Userervlet extends HttpServlet {
      * 使用sesson存储用户登录后返回的对象
      * */
     protected void login(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        req.setCharacterEncoding("utf-8"); //设置请求信息的字符为utf-8
+        resp.setCharacterEncoding("utf-8");
+        resp.setContentType("text/html;charset=UTF-8");
+        boolean boo=true;
+        if (boo){
+            req.getRequestDispatcher("open-list.jsp").forward(req,resp);
+        }else {
+            req.getRequestDispatcher("index.jsp");
+        }
     }
 }
