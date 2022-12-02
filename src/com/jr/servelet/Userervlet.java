3d7b2f44b0d8dd71af28d2d33b2789bf039771e2
wzz <2838259857@qq.com>
2022-12-02 22:24:44 +0800

    登录

--- src/com/jr/servelet/Userervlet.java
+++ src/com/jr/servelet/Userervlet.java
@@ -9,19 +9,13 @@ import java.io.IOException;
 public class Userervlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.setCharacterEncoding("utf-8"); //设置请求信息的字符为utf-8
-        resp.setCharacterEncoding("utf-8");
-        resp.setContentType("text/html;charset=UTF-8");
-        String str=req.getParameter("i");
-        while (str==null){
             login(req,resp);
-        }
 
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+//        super.doPost(req, resp);
     }
 
     @Override
@@ -33,14 +27,6 @@ public class Userervlet extends HttpServlet {
      * 使用sesson存储用户登录后返回的对象
      * */
     protected void login(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.setCharacterEncoding("utf-8"); //设置请求信息的字符为utf-8
-        resp.setCharacterEncoding("utf-8");
-        resp.setContentType("text/html;charset=UTF-8");
-        boolean boo=true;
-        if (boo){
-            req.getRequestDispatcher("open-list.jsp").forward(req,resp);
-        }else {
-            req.getRequestDispatcher("index.jsp");
-        }
+
     }
 }
