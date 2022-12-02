04365ee28ffcfe3ca501c544b31109636ee26028
ji <2301861732@qq.com>
2022-12-02 14:56:17 +0800

    dao包方法完成

--- src/com/jr/dao/impl/ReviewrecordDaoImpl.java
+++ src/com/jr/dao/impl/ReviewrecordDaoImpl.java
@@ -20,14 +20,40 @@ public class ReviewrecordDaoImpl implements IReviewrecordDao {
      * */
     @Override
     public int insertReviewrecord(Reviewrecord reviewrecord) {
-        return 0;
+    String sql="insert into review_record values(?,?,?,?,?,?)";
+    int i=upd(sql);
+    return i;
+
     }
     /**
      * 根据开单id查询审核记录信息
      * */
     @Override
     public Reviewrecord queryReviewrecord(int ticketId) {
-        return null;
+        try {
+            con=DBHelper.getconn();
+            String sql="select r.*,t.id from review_record r,ticket_open t where r.ticket_open_id=t.id";
+            ps=con.prepareStatement(sql);
+            rs=ps.executeQuery();
+            while (rs.next()){
+                reviewrecord1=new Reviewrecord();
+                reviewrecord1.setId(rs.getInt(1));
+                reviewrecord1.setTicketOpenId(rs.getInt(2));
+                reviewrecord1.setCreatorId(rs.getInt(3));
+                reviewrecord1.setCreateTime(rs.getDate(4));
+                reviewrecord1.setReviewStatus(rs.getString(5));
+                reviewrecord1.setRemark(rs.getString(6));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            DBHelper.closeAll(rs,ps,con);
+        }
+        return reviewrecord1;
     }
 
     /**
