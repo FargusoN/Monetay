d733a4a6fb66b6a042ed0bf83952b32f85f7cd19
ji <2301861732@qq.com>
2022-12-03 19:49:07 +0800

    修改

---
+++ src/com/jr/servelet/ReviewdetailServlet.java
@@ -0,0 +1,40 @@
+package com.jr.servelet;
+
+
+
+import com.jr.until.ViewUtility;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/rds")
+public class ReviewdetailServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("utf-8");
+        response.setCharacterEncoding("utf-8");
+        response.setContentType("text/html;charset=utf-8");
+        String no=request.getParameter("no");
+        ViewUtility viewUtility=new ViewUtility();
+        ViewUtility viewUtility1=viewUtility.sleectInfoView(no);
+        HttpSession session = request.getSession();
+        session.setAttribute("viewUtility",viewUtility1);
+        request.getRequestDispatcher("check-detail.jsp").forward(request,response);
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.service(req, resp);
+    }
+}
